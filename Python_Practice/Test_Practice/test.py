f229727e257b9194e2d40367d330494152c28c4d
Vamsi Babu <vamsibabu559@gmail.com>
2026-04-16 15:39:23 +0530

    Update Test_Practice

--- Python_Practice/Test_Practice/test.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-conn=mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Dark@559",
-    database="rl"
-)
-if conn.is_connected():
-    print("connected")
